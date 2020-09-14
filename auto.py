f3a5118d490107f1eebcb7425f14976d760acbb2
LynxKu <64838090+lynxku@users.noreply.github.com>
2020-09-14 11:01:51 +0700

    Add files via upload
    Test

--- auto.py
+++ auto.py
@@ -1,27 +1,6 @@
-import os, requests, getpass
+import requests
+import bs4
 
-os.system('clear')
-print ("========================")
-id = raw_input("Email : ")
-pw = getpass.getpass(promt="Password : ")
-print ("Checking Login...")
-
-url = 'https://mobile.facebook.com/login.php'
-headers = {
-'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0',
-'Accept-Laguage' : 'en-US,en;q=0.5'
-}
-
-data = {
-'email' : id,
-'pass' : pw
-}
-
-requests = requests.post(url, headers=headers, data=data).text
-
-if "xc_message" in requests:
-    print ("Berhasil login")
-elif "checkpointSubmitButton" in requests:
-    print ("Checkpoint")
-else:
-    print ("Login gagal")
+res = requests.get('https://learncodeonline.in')
+type(res)
+res.text
