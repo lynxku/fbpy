9c61630ef9b098986fff05f5bc92b83d88a2a2be
LynxKu <64838090+lynxku@users.noreply.github.com>
2020-09-14 11:39:16 +0700

    Add files via upload

--- auto.py
+++ auto.py
@@ -1,9 +1,21 @@
 import requests
 import bs4
+import os
+import sys
 
+logo = ("Xzylus Remote Website")
+
+os.system('clear')
+print(logo)
 print("Example : https://xzylus.com/shell.php")
-url = input('Url >>>')
+url = input('Url >>> ')
 res = requests.get(url)
 type(res)
 output = res.text
 print(output)
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+p = soup.select('title')
+h1 = p[0].getText()
+print("")
+print("<<<=====Title=====>>>")
+print(">>> " + h1 + " <<<")
