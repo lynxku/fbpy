b3cf0a76a76cca9f4b1d264da462c4ac9d8006d2
LynxKu <64838090+lynxku@users.noreply.github.com>
2020-09-14 11:22:53 +0700

    Add files via upload

--- auto.py
+++ auto.py
@@ -1,7 +1,9 @@
 import requests
 import bs4
 
-res = requests.get('https://learncodeonline.in')
+print("Example : https://xzylus.com/shell.php")
+url = input('Url >>>')
+res = requests.get(url)
 type(res)
 output = res.text
 print(output)
